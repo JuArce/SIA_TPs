f4d3b8da9fb250cc9b535f926a36c7015c54c03f
Julian Arce <juarce@itba.edu.ar>
2022-03-10 11:41:43 -0300

    fix: path to config.json. config.json initial settings.

--- TP1/src/main.py
+++ TP1/src/main.py
@@ -1,1 +1,12 @@
+import json
+
 print("main")
+
+
+f = open('./resources/config.json', "r")
+data = json.load(f)
+print(data)
+# Una clase por algoritmo
+# Cargar la cfg
+# llamar al algoritmo correspondiente
+#
