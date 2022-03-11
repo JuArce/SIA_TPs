927fe045f5f05056ab63d5d76e1cb40ed7c06961
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-03-11 15:17:33 -0300

    fix: node

--- TP1/src/algorithms/Node.py
+++ TP1/src/algorithms/Node.py
@@ -2,8 +2,8 @@ class Node:
 
     def __init__(self, state):
         self.state = state  # que id es (es el estado)
-        self.col_idx = state.
-        self.row_idx = state.row
+        self.col_idx = state.col_idx
+        self.row_idx = state.row_idx
         self.is_visited = False
         self.children = []
 
