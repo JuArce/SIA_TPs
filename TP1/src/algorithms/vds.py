565e8f91da32004a637946d4fa4346b2da78d250
Julian Arce <juarce@itba.edu.ar>
2022-03-13 14:03:36 -0300

    fix: vds implementation.
    Not working yet.

--- TP1/src/algorithms/vds.py
+++ TP1/src/algorithms/vds.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from utils.Config import Config
 from algorithms.Node import Node
 from algorithms.State import State
@@ -12,50 +13,45 @@ def vds(config: Config):
     deep = 0
     used_depths = set()
     vds_depth = int(config.initial_depth)
-    solution = None
+    solution: Optional[Node] = None
 
     while vds_depth not in used_depths:
         used_depths.add(vds_depth)
-        print("added:" + vds_depth.__str__())
         while len(frontier) > 0:
             node = frontier.pop()
+
+            successors = []
+            # La profundidad de la solucion tiene que ser siempre menor que la que ya se habia obtenido
+            if not solution or (solution and node.deep < solution.deep):
+                if node.state.id == config.final_state:
+                    solution = node
+                    deep = node.deep
+                    result = True
+                    break
+
             successors = Plays.get_moves(node, ex)
             ex.add(node)
-
-            # tenemos que pasar al siguiente nodo de la frontera
-            if node.state.id == config.final_state:
-                deep = node.deep
-                result = True
-                solution = node
-                break
-
-            if node.deep > deep:
-                deep = node.deep
-
             expanded_nodes += 1
-
-            if node.deep > vds_depth:
-                for s in successors:
-                    state = State(s)
-                    child = Node(state, node)
-                    node.children.append(child)
-                    if child not in ex:
-                        frontier.appendleft(child)
-                continue
 
             for s in successors:
                 state = State(s)
                 child = Node(state, node)
                 node.children.append(child)
                 if child not in ex:
-                    frontier.append(child)
+                    if child.deep <= vds_depth:
+                        frontier.appendleft(child)
+                    else:
+                        frontier.append(child)
+
         if result:
+            print("Bajando deep" + deep.__str__())
             vds_depth = deep - 1
         else:
+            print("Subo deep " + vds_depth.__str__())
             vds_depth += 1
 
     time = datetime.now() - time
-    cost = vds_depth
+    cost = deep
 
     plays_to_win = Plays.get_plays_to_win(solution) if result else None
 
