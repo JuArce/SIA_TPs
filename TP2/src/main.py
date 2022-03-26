e754e0ed1182c5523b9fddf10b4f66781ccbe0dd
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-03-26 15:38:11 -0300

    config: add main and bag

--- TP2/src/main.py
+++ TP2/src/main.py
@@ -1,12 +1,22 @@
+import array
 import sys
 
 print('Argument List:', str(sys.argv))
 assert len(sys.argv) == 2, 'Missing config json'
 
+max_weight: int
+total_items: int
 
-f = open(sys.argv[1], 'r')
+with open(sys.argv[1], 'r') as f:
+    line = f.readline()
+    aux: list[str] = line.split(',')
+    total_items = int(aux[0])
+    max_weight = int(aux[1])
+
+    while line:
+        line = f.readline()
+        aux: list[str] = line.split(',')
 
 
-
-
-f.close()
+        print(line)
+    f.close()
--- TP2/src/population/Bag.py
+++ TP2/src/population/Bag.py
@@ -3,11 +3,8 @@ from Element import Element
 
 class Bag:
 
-
-    def __init__(self, elements: Elements):
+    def __init__(self, elements: Element):
         self.elements = elements
-        self.total_value,self.total_weight = self.get_total_value_and_weight()
+        # self.total_value,self.total_weight = self.get_total_value_and_weight()
 
-    def get_total_value_and_weight(self):
-
-        for
+    # def get_total_value_and_weight(self):
