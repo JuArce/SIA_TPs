b04658b39cc2ec704a632e6fc16b7b6dc67f122a
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-03-26 20:48:43 -0300

    feat: add uniform cross

--- TP2/src/cross_over/uniform.py
+++ TP2/src/cross_over/uniform.py
@@ -1,3 +1,18 @@
-def uniform():
-    return None
+import random
 
+
+def uniform(chromosomes: [str]):
+    aux = [[], []]
+
+    for i in range(len(chromosomes[0])):
+        p = random.random()
+        # Hay que intercambiar si p < 0.5
+        if p < 0.5:
+            aux[0].append(chromosomes[1][i])
+            aux[1].append(chromosomes[0][i])
+        else:
+            aux[0].append(chromosomes[0][i])
+            aux[1].append(chromosomes[1][i])
+    output = ["".join(aux[0]), "".join(aux[1])]
+
+    return output
