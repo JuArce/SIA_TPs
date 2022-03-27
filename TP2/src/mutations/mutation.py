e9749ad35dba288fff7cef4bb7c0681138f9c341
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-03-27 16:15:48 -0300

    feat: add probability to mutation
    Co-authored-by: JULIAN ARCE <juarce@itba.edu.ar>
    Co-authored-by: GIAN PECILE <gpecile@itba.edu.ar>

--- TP2/src/mutations/mutation.py
+++ TP2/src/mutations/mutation.py
@@ -1,13 +1,13 @@
 import random
 
 
-def mutation(chromosome: str):
+def mutation(chromosome: str, mutation_probability):
     output = []
 
     for i in range(len(chromosome)):
         p = random.random()
         # Hay que mutar si p < 0.05
-        if p < 0.05:
+        if p < mutation_probability:
             aux = "1" if chromosome[i] == '0' else "0"
             output.append(aux)
         else:
