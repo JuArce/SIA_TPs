a98bfc112d8a95d5f011d0c4dd8d099e94e120f5
Julian Arce <juarce@itba.edu.ar>
2022-03-26 21:04:03 -0300

    feat: multiple cross method.

--- TP2/src/cross_over/multiple.py
+++ TP2/src/cross_over/multiple.py
@@ -1,3 +1,22 @@
-def multiple():
-    return None
+import random
 
+
+def multiple(chromosomes: [str], points: int):
+    p = random.sample(range(len(chromosomes[0]) - 1), points)
+    p.sort()
+
+    output: [str] = [[], []]
+    prev = 0
+    idx_0 = 0
+    idx_1 = 1
+    for i in p:
+        output[0].append(chromosomes[idx_0][prev: i])
+        output[1].append(chromosomes[idx_1][prev: i])
+        prev = i
+        idx_0, idx_1 = idx_1, idx_0
+    output[0].append(chromosomes[idx_0][prev: len(chromosomes[0])])
+    output[1].append(chromosomes[idx_1][prev: len(chromosomes[1])])
+
+    output[0] = ''.join(output[0])
+    output[1] = ''.join(output[1])
+    return output
