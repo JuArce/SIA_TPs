e43e6b64ebd837dff767d5ec1b424a4c4773d661
Julian Arce <juarce@itba.edu.ar>
2022-03-29 13:50:10 -0300

    feat: roulette selection implemented.

--- TP2/src/selection/roulette.py
+++ TP2/src/selection/roulette.py
@@ -1,4 +1,19 @@
+import copy
+import random
+
 from TP2.src.utils.Config import Config
 
+
 def roulette(chromosomes: dict, config: Config):
-    return None
+    elements = copy.deepcopy(chromosomes)
+    output = dict()
+    while len(output.keys()) < config.population:
+        selected = random.choices(list(elements.items()),
+                                  weights=list(elements.values()),
+                                  k=(config.population - len(output.keys())))
+        output.update(selected)
+        for k in selected:
+            if k[0] in elements:
+                elements.pop(k[0])
+
+    return output
