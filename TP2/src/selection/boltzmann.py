be3cb424ed002fa0452cf37fe25c534d10e550e2
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-03-31 11:50:02 -0300

    feat: add temperature in boltzmann selectino

--- TP2/src/selection/boltzmann.py
+++ TP2/src/selection/boltzmann.py
@@ -1,5 +1,14 @@
+import math
+
 from utils.selection_parameters import SelectionParameter
 
 
 def boltzmann(chromosomes: dict, selection_parameter: SelectionParameter):
-    return None
+    t = get_temperature(selection_parameter)
+
+
+def get_temperature(selection_parameter: SelectionParameter):
+    tc = selection_parameter.temperature_goal
+    to = selection_parameter.initial_temperature
+    factor = selection_parameter.decrease_temp_factor * selection_parameter.current_gen
+    return tc + (to - tc) * math.exp(- factor)
