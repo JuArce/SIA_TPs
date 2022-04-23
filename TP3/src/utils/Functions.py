b02bbbd4cafcf1db7073a6c1a96fe2a224ae54e6
Julian Arce <juarce@itba.edu.ar>
2022-04-23 13:01:55 -0300

    fix: get_error fixed.

--- TP3/src/utils/Functions.py
+++ TP3/src/utils/Functions.py
@@ -13,7 +13,7 @@ def get_error(x: np.array, y: np.array, w: np.array, p: int):
     ret = 0
     while i < p:
         o = x[i] @ w
-        if not math.isclose(o, y[i], abs_tol=0.00001):
+        if not math.isclose(sign(o), y[i], abs_tol=0.00001):
             ret = ret + 1
         i = i + 1
 
