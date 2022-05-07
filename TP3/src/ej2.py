8957a7cd8c2bdf15498487c3c8095bff0394eaa6
Julian Arce <juarce@itba.edu.ar>
2022-05-07 18:39:41 -0300

    feat: graph improvements. Not linear perceptron graphs.

--- TP3/src/ej2.py
+++ TP3/src/ej2.py
@@ -9,8 +9,9 @@ import numpy as np
 
 from algorithms.Perceptron import SimplePerceptron, NoLinearPerceptron, LinearPerceptron
 from utils.Config_p import Config
-from utils.Graph import graph
+from utils.Graph import graph, graph_table
 from utils.PerceptronParameters import PerceptronParameters
+from utils.Utils import build_train
 
 
 def __main__():
@@ -82,21 +83,47 @@ def __main__():
     results_test = []
     points = []
     colors = []
+    cell_text = []
+    train_errors = []
+    test_errors = []
+    train_stdev = []
+    test_stdev = []
+
+    columns = []
+    for i in range(k):
+        columns.append(str(i + 1))
+    rows = ['Error Train', 'Std Dev Train', 'Error Test', 'Std Dev Test']
+
     for i in range(k):
         # reiniciamos el perceptron
         perceptron.__init__(perceptron_parameters)
+
         training_x, training_y, testing_x, testing_y = build_train(indexes, x, y, i)
+
         r_train = perceptron.train(training_x, training_y)
         r_test = perceptron.predict(testing_x, testing_y)
-        results_training.append(r_train)
-        results_test.append(r_test)
-        points.append([i, r_test])
-        colors.append('#fa0000')  # Red -> PREDICT
+
+        # results_training.append(r_train)
+        # results_test.append(r_test)
+
         points.append([i, r_train.errors[-1]])
         colors.append('#00ff3c')  # Green -> TRAIN
 
+        points.append([i, r_test])
+        colors.append('#fa0000')  # Red -> PREDICT
+
+        train_errors.append(r_train.errors[-1])
+        test_errors.append(r_test)
+        train_stdev.append(get_3d_stddev(training_x))
+        test_stdev.append(get_3d_stddev(testing_x))
+
+    cell_text.append([round(n, 4) for n in train_errors])
+    cell_text.append([round(n, 4) for n in train_stdev])
+    cell_text.append([round(n, 4) for n in test_errors])
+    cell_text.append([round(n, 4) for n in train_stdev])
     graph(x_label='k', y_label='error', title='Train (Green) vs Test (Red)', points=numpy.array(points),
           points_color=colors)
+    graph_table(cell_text=cell_text, rows=rows, columns=columns)
     # normalizar la salida del no lineal
 
     # Seleccionar cuál es el mejor betha para entrenar a la red
@@ -109,11 +136,14 @@ def __main__():
         train_aux(perceptron, x, y, bethas[i], 'tanh', errors_tanh, aux_parameters)
         train_aux(perceptron, x, y, bethas[i], 'logistic', errors_logistic, aux_parameters)
 
+    graph(bethas, errors_logistic, 'Betha', 'Error', 'Errores para distintos bethas (logistic)')
+    graph(bethas, errors_tanh, 'Betha', 'Error', 'Errores para distintos bethas (tanh)')
+
+
     # Seleccionar qué porcentaje es mejor tomar de población para entrenar y para testeo
-    errors_train = []
-    std_dev_train = []
-    errors_test = []
-    std_dev_test = []
+    points = []
+    errors = []
+    colors = []
 
     percentages = [0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90]
     for i in range(len(percentages)):
@@ -131,13 +161,19 @@ def __main__():
             perceptron.__init__(perceptron_parameters)
             r_train = perceptron.train(training_x, training_y)
             r_test = perceptron.predict(testing_x, testing_y)
+
             i_train_errors.append(r_train.errors[-1])
             i_test_errors.append(r_test)
 
-        errors_train.append(mean(i_train_errors))
-        std_dev_train.append(stdev(i_train_errors))
-        errors_test.append(mean(i_test_errors))
-        std_dev_test.append(stdev(i_test_errors))
+        points.append([percentages[i], mean(i_train_errors)])
+        errors.append(stdev(i_train_errors))
+        colors.append('#00ff3c')  # Green -> TRAIN
+
+        points.append([percentages[i], mean(i_test_errors)])
+        errors.append(stdev(i_test_errors))
+        colors.append('#fa0000')  # Red -> PREDICT
+
+    graph(points=numpy.array(points), points_color=colors, e=errors)
 
     print(config.perceptron_algorithm + ' finished.')
 
@@ -148,6 +184,14 @@ def train_aux(perceptron, x, y, betha, function, errors, parameters):
     perceptron.__init__(parameters)
     r_train = perceptron.train(x, y)
     errors.append(r_train.errors[-1])
+
+    
+def get_3d_stddev(x):
+    x_axis_stdev = stdev(x[:, 0])
+    y_axis_stdev = stdev(x[:, 1])
+    z_axis_stdev = stdev(x[:, 2])
+    return (x_axis_stdev + y_axis_stdev + z_axis_stdev) / 3
+
 
 
 if __name__ == "__main__":
--- TP3/src/utils/Graph.py
+++ TP3/src/utils/Graph.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-def graph(x=None, y=None, x_label='', y_label='', title='', points=None, points_color=None, e=None, output_dir=None):
+def graph(x=None, y=None, x_label='', y_label='', title='', points=None, points_color=None, e=None, output_dir=None,
+          cell_text=None, rows=None, columns=None):
     graph_init()
 
     if points is not None and points_color is not None:
         if e is not None:
-            graph_points_with_std_dev(points[:, 0], points[:, 1], e)
+            graph_points_with_std_dev(points[:, 0], points[:, 1], e, points_color)
         else:
             graph_points(points[:, 0], points[:, 1], points_color)
 
     if x is not None and y is not None:
         graph_plot(x, y)
+
+    if cell_text is not None:
+        graph_table_aux(cell_text, rows, columns)
 
     graph_description(x_label, y_label, title)
 
@@ -21,9 +25,27 @@ def graph(x=None, y=None, x_label='', y_label='', title='', points=None, points_
         graph_show()
 
 
+def graph_table(cell_text=None, rows=None, columns=None, output_dir=None):
+    graph_clear()
+    plt.figure(dpi=400)
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.box(on=None)
+    plt.subplots_adjust(left=0.2, bottom=0.8)
+
+    if cell_text is not None:
+        graph_table_aux(cell_text, rows, columns)
+
+    if output_dir:
+        graph_save(output_dir)
+    else:
+        graph_show()
+
+
 def graph_init(width=7, height=7):
     plt.clf()
-    plt.figure(figsize=(width, height), layout='constrained', dpi=200)
+    plt.figure(figsize=(width, height), layout='constrained', dpi=300)
 
 
 def graph_clear():
@@ -43,11 +65,12 @@ def graph_plot(x, y):
 
 
 def graph_points(x, y, colors):
-    plt.scatter(x, y, s=100, c=colors, label=colors)
+    plt.scatter(x, y, s=50, c=colors, label=colors)
 
 
 def graph_points_with_std_dev(x, y, e, colors):
-    plt.errorbar(x, y, e)
+    plt.scatter(x, y, s=50, c=colors, label=colors)
+    plt.errorbar(x, y, e, linestyle='None', ecolor='#000000')
 
 
 def graph_description(x_label, y_label, title):
@@ -55,3 +78,11 @@ def graph_description(x_label, y_label, title):
     plt.ylabel(y_label)
     plt.title(title)
     plt.grid(True)
+
+
+def graph_table_aux(cell_text, rows, columns):
+    table = plt.table(cellText=cell_text,
+                      rowLabels=rows,
+                      colLabels=columns,
+                      loc='bottom')
+    table.scale(1, 1.5)
