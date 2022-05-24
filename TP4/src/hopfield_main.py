1a7f7a6bffb04666da063e8708518e1909203585
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-05-24 12:11:20 -0300

    feat: choose best pattern to train

--- TP4/src/hopfield_main.py
+++ TP4/src/hopfield_main.py
@@ -1,3 +1,5 @@
+import itertools
+import string
 import sys
 
 import numpy as np
@@ -7,6 +9,21 @@ from utils.ConfigULK import Config
 from utils.HopfieldParameters import HopfieldParameters
 
 
+def get_all_products(letter_combinations, letters_dict):
+    letters_dict_comb = dict()
+    for i in range(len(letter_combinations)):
+        key = ','.join(letter_combinations[i])
+        aux = list(itertools.combinations(letter_combinations[i], 2))
+        prod = 0
+        for j in range(len(aux)):
+            prod += abs(np.dot(letters_dict[aux[j][0]], letters_dict[aux[j][1]]))
+        letters_dict_comb[key] = prod
+
+    ordered_dict = dict(sorted(letters_dict_comb.items(), key=lambda item: item[1]))
+
+    return ordered_dict
+
+
 def main():
     print('Argument List:', str(sys.argv))
     assert len(sys.argv) == 3, 'Missing arguments'
@@ -14,7 +31,12 @@ def main():
     config: Config = Config(f.read())
     f.close()
 
-    x = []
+    # Get letters representation
+
+    letters_array = list(string.ascii_uppercase)
+    letters_dict = dict()
+    letter_idx = 0
+
     with open(sys.argv[2], 'r') as inputs_file:
         i = 1
         aux = []
@@ -24,15 +46,23 @@ def main():
                 for e in values:
                     aux.append(1 if e == '*' else -1)
                 if i % 5 == 0:
-                    x.append(aux)
+                    letters_dict[letters_array[letter_idx]] = np.array(aux)
+                    letter_idx += 1
                     aux = []
                 i += 1
 
-    x = np.array(x)
+    ## Get all posibilities of len 4
+    letter_combinations = list(itertools.combinations(letters_array, 4))
+    letter_products = get_all_products(letter_combinations, letters_dict)
+
+    first = next(iter(letter_products))
+    patterns_letters = first.split(',')
+    patterns_values = []
+    for i in patterns_letters:
+        patterns_values.append(letters_dict[i])
 
     parameters = HopfieldParameters(config)
-
-    hopfield = Hopfield(parameters, x)
+    hopfield = Hopfield(parameters, np.array(patterns_values))
 
 
 if __name__ == '__main__':
