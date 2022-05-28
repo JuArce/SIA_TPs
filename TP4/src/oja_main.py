63e8833a4698ef189330d451f8d145571eecf3c7
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-05-28 18:19:25 -0300

    feat: add results to file oja

--- TP4/src/oja_main.py
+++ TP4/src/oja_main.py
@@ -17,7 +17,7 @@ eigenvectors_c = ['1', '2', '3', '4', '5', '6', '7']
 def main():
     np.set_printoptions(suppress=True)
     print('Argument List:', str(sys.argv))
-    assert len(sys.argv) == 3, 'Missing arguments'
+    assert len(sys.argv) == 4, 'Missing arguments'
     f = open(sys.argv[1])
     config: Config = Config(f.read())
     f.close()
@@ -51,7 +51,22 @@ def main():
     f_c_o = np.matmul(standardize_data, results.w[-1])
     # A cada w obtenido le resto el autovector asociado a la primera componente para comparar el error en cada
     # iteracion de w
-    errors = results.w - f_e
+    errors = abs(results.w[-1]) - abs(f_e)
+    f = open(sys.argv[3], 'w')
+    aux = [f_e, results.w[-1], abs(errors)]
+    f.write('Eigenvector\n')
+    aux = pd.DataFrame(data=np.transpose(aux), index=df.columns.values, columns=['Library', 'Oja', 'Error'])
+    f.write(aux.to_string())
+    f.write('\n\n#########################\n\n')
+
+    errors = abs(principal_components[:, 0]) - abs(f_c_o)
+    aux = [principal_components[:, 0], f_c_o, abs(errors)]
+    f.write('First Component\n')
+    aux = pd.DataFrame(data=np.transpose(aux), index=df.index.values,
+                       columns=['Library', 'Oja', 'Error'])
+    f.write(aux.to_string())
+
+    f.close()
 
 
 if __name__ == '__main__':
