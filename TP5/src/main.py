2d52e444ad5d7025c030ff43052f50eab0bf28ce
Agustin Manfredi <imanfredi@itba.edu.ar>
2022-06-19 01:11:40 -0300

    feat: add multiple new letters

--- TP5/src/main.py
+++ TP5/src/main.py
@@ -9,6 +9,50 @@ from utils.fonts import font_2
 from utils.utils import to_bin_array, resize_letter, midpoint
 
 
+def generate_new_letters(letters_dict, autoencoder, letters):
+    graphs = []
+    indexs = np.random.choice(len(letters), 2, replace=False)
+
+    letter_1 = letters[indexs[0]]
+    letter_2 = letters[indexs[1]]
+
+    letter_1_array = np.concatenate(letters_dict[letter_1])
+    letter_2_array = np.concatenate(letters_dict[letter_2])
+    letter_1_encode = autoencoder.encode(letter_1_array)
+    letter_2_encode = autoencoder.encode(letter_2_array)
+
+    direction = letter_2_encode - letter_1_encode  # de 1 a 2
+
+    parts = 4
+
+    proportion = direction / parts
+
+    graphs.append(letters_dict[letter_1])
+
+    letter_1_decode = autoencoder.decode(letter_1_encode)
+    res = np.array(list(map(resize_letter, [letter_1_decode])))
+    graphs.append(res[0])
+
+    for i in range(1, parts):
+        letter_3_encode = letter_1_encode + proportion * i
+        letter_3_decode = autoencoder.decode(letter_3_encode)
+        res = np.array(list(map(resize_letter, [letter_3_decode])))
+        graphs.append(res[0])
+
+    letter_3_encode = midpoint(letter_1_encode[0], letter_1_encode[1], letter_2_encode[0], letter_2_encode[1])
+    letter_3_decode = autoencoder.decode(letter_3_encode)
+    res = np.array(list(map(resize_letter, [letter_3_decode])))
+    graphs.append(res[0])
+
+    letter_2_decode = autoencoder.decode(letter_2_encode)
+    res = np.array(list(map(resize_letter, [letter_2_decode])))
+    graphs.append(res[0])
+
+    graphs.append(letters_dict[letter_2])
+
+    SeaGraphV2.graph_multi_heatmap(graphs, title='New Letter', c_map="Greys", cols=4)
+
+
 def __main__():
     print('Argument List:', str(sys.argv))
     assert len(sys.argv) == 2, 'Missing arguments'
@@ -17,9 +61,8 @@ def __main__():
     f.close()
     data = []
     letters_patterns = []
-    # letters = ['@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    #            'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_']
-    letters = ['A', 'B', 'C', 'D', 'E', '_']
+    letters = ['@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+               'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_']
     for letter in font_2:
         aux = to_bin_array(letter)
         data.append(np.concatenate(aux))
@@ -50,31 +93,8 @@ def __main__():
             graphs = []
 
     # NEW LETTERS
-    graphs = []
-    letter_1 = 'A'
-    letter_2 = 'B'
-
-    letter_1_array = np.concatenate(letters_dict[letter_1])
-    letter_2_array = np.concatenate(letters_dict[letter_2])
-    letter_1_encode = autoencoder.encode(letter_1_array)
-    letter_2_encode = autoencoder.encode(letter_2_array)
-
-    graphs.extend([letters_dict[letter_1], letters_dict[letter_2]])
-
-    letter_3_encode = midpoint(letter_1_encode[0], letter_1_encode[1], letter_2_encode[0], letter_2_encode[1])
-    letter_3_decode = autoencoder.decode(letter_3_encode)
-    res = np.array(list(map(resize_letter, [letter_3_decode])))
-    graphs.append(res[0])
-
-    letter_1_decode = autoencoder.decode(letter_1_encode)
-    res = np.array(list(map(resize_letter, [letter_1_decode])))
-    graphs.append(res[0])
-
-    letter_2_decode = autoencoder.decode(letter_2_encode)
-    res = np.array(list(map(resize_letter, [letter_2_decode])))
-    graphs.append(res[0])
-
-    SeaGraphV2.graph_multi_heatmap(graphs, title='New Letter', c_map="Greys", cols=3)
+    for i in range(6):
+        generate_new_letters(letters_dict, autoencoder, letters)
 
 
 if __name__ == "__main__":
